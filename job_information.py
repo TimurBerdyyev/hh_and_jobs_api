ecb2c0694f3955ed8490aa64e1b7701a9ceec8a0
Тимур <159126505+TimurBerdyyev@users.noreply.github.com>
2024-05-01 11:46:47 +0600

    Update job_information.py

--- job_information.py
+++ job_information.py
@@ -5,7 +5,7 @@ import vacancy_analysis_math
 
 def main():
     hh_stats = hh_api.analyze_vacancies()
-    super_job_stats = super_job_api.analyze_vacancies()
+    super_job_stats = super_job_api.analyze_superjob_vacancies()
 
     print("HeadHunter Statistics:")
     vacancy_analysis_math.print_stats_table(hh_stats)
